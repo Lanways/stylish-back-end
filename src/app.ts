470528145a9c0bf4864922f1a0acb1a44bf790fa
Lanways <mcbluedd@gmail.com>
2023-12-12 01:34:22 +0800

    feat: install cors

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,16 @@
 import express, { Request, Response } from 'express'
+import router from './routes'
+import cors from 'cors'
+
 const app = express()
-const PORT = process.env.PORT || 3002
+const PORT = process.env.PORT || 3000
+const corsOptions = {
+  origin: ['http://localhost:3001'],
+  credentials: true,
+}
+
+app.use(cors(corsOptions))
+app.use(router)
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World')
