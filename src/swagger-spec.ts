b3bb108f3dbf81cc35cb52a63f17299e8866fdde
Lanways <mcbluedd@gmail.com>
2023-12-21 04:11:57 +0800

    feat: add postProduct route and docs

--- src/swagger-spec.ts
+++ src/swagger-spec.ts
@@ -5,7 +5,66 @@ const doc = {
     title: 'Stylish',
     description: 'Description'
   },
-  host: 'localhost:3000'
+  host: 'localhost:3000',
+  definitions: {
+    postProduct: {
+      status: "200",
+      message: "OK",
+      data: {
+        id: 8,
+        name: 'Cape Lion',
+        price: 999,
+        image: "http://123123",
+        sizeOptions: "S",
+        quantity: 2,
+        description: "123",
+        additionalImage: "http://232323.456",
+        updatedAt: "2023-12-20T18:02:15.641Z",
+        createdAt: "2023-12-20T18:02:15.641Z"
+      }
+    },
+    productBody: {
+      name: 'Cape Lion',
+      price: 999,
+      image: "http://123123",
+      sizeOptions: "S",
+      quantity: 2,
+      description: "123",
+      additionalImage: "http://232323.456",
+    },
+    getProductById: {
+      status: "200",
+      message: "OK",
+      data: {
+        id: 7,
+        name: "Cape lion",
+        price: "192",
+        createdAt: "2023-12-14T06:47:26.000Z",
+        updatedAt: "2023-12-14T06:47:26.000Z",
+        image: "https://loremflickr.com/640/480/clothing?lock=1957181607903232",
+        sizeOptions: "S",
+        quantity: 1,
+        description: "Viduo verecundia ambitus convoco aestas.Civis utrimque conturbo.",
+        additionalImage: "https://loremflickr.com/640/480/clothing?lock=122186496999424"
+      }
+    },
+    getProducts: {
+      status: "200",
+      message: "OK",
+      data: [{
+        id: 7,
+        name: "Cape lion",
+        price: "192",
+        createdAt: "2023-12-14T06:47:26.000Z",
+        updatedAt: "2023-12-14T06:47:26.000Z",
+        image: "https://loremflickr.com/640/480/clothing?lock=1957181607903232",
+        sizeOptions: "S",
+        quantity: 1,
+        description: "Viduo verecundia ambitus convoco aestas.Civis utrimque conturbo.",
+        additionalImage: "https://loremflickr.com/640/480/clothing?lock=122186496999424"
+      },]
+    }
+  }
 };
 
 const outputFile = './src/swagger-output.json';
