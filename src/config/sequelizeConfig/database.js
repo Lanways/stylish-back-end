5eb7c85a9cb36c66aee5c291965a60389ae84f96
Lanways <mcbluedd@gmail.com>
2023-12-28 22:00:39 +0800

    refactor: change database config to module.export

--- src/config/sequelizeConfig/database.js
+++
@@ -1,8 +0,0 @@
-// const config = require('../').config
-// import { config } from '../database'
-
-const config = require('../../../dist/config/database').config
-
-// console.log(config)
-
-module.exports = config
