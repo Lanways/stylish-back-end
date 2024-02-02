d1f5352e49899fcd1aa7a1bed0a202b9308b58e0
Lanways <mcbluedd@gmail.com>
2024-02-02 23:58:26 +0800

    feat: create run sequelize cli script

---
+++ src/run-sequelizecli.ts
@@ -0,0 +1,29 @@
+import {
+  SecretsManagerClient,
+  GetSecretValueCommand,
+} from "@aws-sdk/client-secrets-manager"
+
+const execSync = require("child_process").execSync;
+const secretName = "stylish-env-variable";
+const client = new SecretsManagerClient();
+const sequelizeCommand = process.argv[2] || "db:migrate"
+
+async function getSecrets() {
+  try {
+    const command = new GetSecretValueCommand({ SecretId: secretName })
+    const { SecretString } = await client.send(command);
+    if (SecretString) {
+      const secrets = JSON.parse(SecretString);
+      process.env.DB_USER = secrets.DB_USER;
+      process.env.DB_PASS = secrets.DB_PASS;
+      process.env.DB_NAME = secrets.DB_NAME;
+      process.env.DB_HOST = secrets.DB_HOST;
+      execSync(`npx sequelize ${sequelizeCommand}`, { stdio: "inherit" })
+    }
+  } catch (error) {
+    console.error("Error fetching secrets:", error)
+    process.exit(1)
+  }
+}
+
+getSecrets();
