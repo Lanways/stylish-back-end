e30e69c5a29af3c7c5c21a14b6c85bc37b6b8b91
Lanways <mcbluedd@gmail.com>
2024-02-28 00:52:39 +0800

    feat: add order model and migration

---
+++ src/db/models/order.ts
@@ -0,0 +1,76 @@
+import { Model, Optional, Sequelize, DataTypes } from "sequelize"
+import db from './index'
+
+interface OrderAttributes {
+  id: number
+  userId: number
+  shippingFeeId: number
+  totalPrice: number
+  status: string
+  createdAt: Date
+  updatedAt: Date
+}
+
+interface OrderInput extends Optional<OrderAttributes, 'id' | 'createdAt' | 'updatedAt'> { }
+export interface OrderOutput extends Required<OrderAttributes> { }
+
+class Order extends Model<OrderAttributes, OrderInput> implements OrderAttributes {
+  static associate(model: typeof db) {
+    Order.belongsTo(model.User, { foreignKey: 'userId' })
+    Order.belongsTo(model.ShippingFee, { foreignKey: 'shippingFeeId' })
+    Order.hasOne(model.Shipping, { foreignKey: 'orderId' })
+    Order.hasOne(model.Payment, { foreignKey: 'orderId' })
+    Order.hasMany(model.OrderItem, { foreignKey: 'orderId' })
+  }
+  id!: number
+  userId!: number
+  shippingFeeId!: number
+  totalPrice!: number
+  status!: string
+  createdAt!: Date
+  updatedAt!: Date
+}
+
+const orderInit = (sequelize: Sequelize) => {
+  Order.init({
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    userId: {
+      allowNull: false,
+      type: DataTypes.INTEGER
+    },
+    shippingFeeId: {
+      allowNull: false,
+      type: DataTypes.INTEGER
+    },
+    totalPrice: {
+      allowNull: false,
+      type: DataTypes.DECIMAL
+    },
+    status: {
+      allowNull: false,
+      type: DataTypes.ENUM,
+      values: ['Pending', 'Shipped', 'Delivered', 'Cancelled']
+    },
+    createdAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: DataTypes.DATE
+    }
+  }, {
+    sequelize,
+    modelName: 'Order',
+    tableName: 'Orders',
+    underscored: true
+  })
+  return Order
+}
+
+export default orderInit
