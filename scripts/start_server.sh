d0dd0845ea4205b3b58a59c48720d663600d37d3
Lanways <mcbluedd@gmail.com>
2024-03-20 21:40:40 +0800

    feat: add app container on port 3001

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 echo "Start app container"
-docker run -d -p 80:80 -e NODE_ENV=production --name stylish_app_container stylish/stylish:v1
+docker run -d -p 3000:3000 -e PORT=3000 -e NODE_ENV=production --name stylish_app_container stylish/stylish:v1
+docker run -d -p 3001:3001 -e PORT=3001 -e NODE_ENV=production --name stylish_app_container2 stylish/stylish:v1
